ff9548268a5a2b2202380163be950fe396778bb6
kondaiah <kondaiahyadav567@gmail.com>
2024-12-11 22:45:13 +0530

    food ordering app

---
+++ src/main/java/com/ltim/online/food/order/OnlineFoodOrderApplication.java
@@ -0,0 +1,13 @@
+package com.ltim.online.food.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderApplication.class, args);
+	}
+
+}
